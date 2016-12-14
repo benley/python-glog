4dc37a488087bdcfe608c2d5a98275d3c3d8240a
Benjamin Staffin <benley@gmail.com>
2016-12-13 20:36:59 -0500

    Bump version, add six dep, bump gflags version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 import os
 import setuptools
 
-VERSION = '0.3'
+VERSION = '0.3.1'
 
 README = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 
@@ -12,7 +12,11 @@ setuptools.setup(
     author='Benjamin Staffin',
     author_email='benley@gmail.com',
     url='https://github.com/benley/python-glog',
-    install_requires=['python-gflags>=3.0'],
+    install_requires=[
+        'python-gflags>=3.1',
+        'six',  # glog doesn't need six, but gflags 3.1 does and its distutils
+                # "requires" line apparently accomplishes nothing, so ...
+    ],
     description='Simple Google-style logging wrapper for Python.',
     long_description=README,
     py_modules=['glog'],
